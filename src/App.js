2533a11eeff6c6b1f3e67b3be5b498385845c9cf
francienevaz <fran.vfraga@gmail.com>
2022-03-07 08:30:54 -0300

    testing DidMount, DidUpdate, WillUnmount

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, {Component} from "react";
+import Twitter from "./Twitter";
 
-
-class Twitter extends Component {
-  
-  componentWillUnmount() {
-    console.log('componentWillUnmount')
+class App extends Component {
+  state = {
+    loading: false,
+    actived: true
   }
   componentDidMount() {
-    console.log('componentDidMount')
+    setTimeout(()=> {
+      this.setState({
+        loading: true
+      })
+    }, 3000)
+  }
+
+  onRemove = () => {
+    this.setState({
+      actived: false
+    })
   }
 
   render () {
-    return (
-      <div>
-        testando
-      </div>
-    )   
+         const posts = [{
+        title: 'xpto',
+        description: 'foo'
+      }, {
+        title: 'xpto',
+        description: 'foo'
+      }]
+  
+      return (
+        <div>
+          <button onClick={this.onRemove}>Remover component</button>
+          {this.state.actived && (<Twitter posts={posts}/>)}          
+        </div>
+      )   
+    }
   }
-}
-
-export default Twitter
+  
+  export default App
